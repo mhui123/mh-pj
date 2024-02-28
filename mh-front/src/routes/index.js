97a108c51b61aa0c76e5a6d391159cc091ba94e1
mhui123 <mhui7864@gmail.com>
2024-02-28 16:54:07 +0900

    관리자권한 오작동수정

--- mh-front/src/routes/index.js
+++ mh-front/src/routes/index.js
@@ -8,18 +8,18 @@ const routes = [
   { path: '/signin', component: () => import('@/views/SigninPage.vue') },
   { path: '/user', component: () => import('@/views/UserPage.vue'), meta: { auth: true } },
   { path: '/main', component: () => import('@/views/MainPage.vue') },
-  { path: '/add', component: () => import('@/views/WordAddPage.vue'), meta: { auth: true, role: 'ROLE_USER' || 'ROLE_ADMIN' } },
+  { path: '/add', component: () => import('@/views/WordAddPage.vue'), meta: { auth: true, role: ['ROLE_USER', 'ROLE_ADMIN'] } },
   { path: '/fetch', component: () => import('@/views/FetchPage.vue') },
-  { path: '/edit', component: () => import('@/views/WordAddPage.vue'), meta: { auth: true, role: 'ROLE_USER' || 'ROLE_ADMIN' } },
-  { path: '/mypage', component: () => import('@/views/MyPage.vue'), meta: { auth: true, role: 'ROLE_USER' || 'ROLE_ADMIN' } },
-  { path: '/adminPage', component: () => import('@/views/AdminPage.vue'), meta: { auth: true, role: 'ROLE_ADMIN' } },
+  { path: '/edit', component: () => import('@/views/WordAddPage.vue'), meta: { auth: true, role: ['ROLE_USER', 'ROLE_ADMIN'] } },
+  { path: '/mypage', component: () => import('@/views/MyPage.vue'), meta: { auth: true, role: ['ROLE_USER', 'ROLE_ADMIN'] } },
+  { path: '/adminPage', component: () => import('@/views/AdminPage.vue'), meta: { auth: true, role: ['ROLE_ADMIN'] } },
   //fetchPage
   // { path: '*', component: () => import('@/views/NotFoundPage.vue') },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });
 router.beforeEach((to, from, next) => {
-  if (to.meta.role && to.meta.role !== getItem('role')) {
+  if (to.meta.role && !to.meta.role.includes(getItem('role'))) {
     alert('페이지 권한이 없습니다.');
     next('/main');
     return false;
