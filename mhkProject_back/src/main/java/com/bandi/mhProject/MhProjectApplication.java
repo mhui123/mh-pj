d25d29f55d6703855c27fb6b94c3cfd830aaf1b5
mhui123 <mhui7864@gmail.com>
2024-03-03 14:15:16 +0900

    war파일 세팅

--- mhkProject_back/src/main/java/com/bandi/mhProject/MhProjectApplication.java
+++ mhkProject_back/src/main/java/com/bandi/mhProject/MhProjectApplication.java
@@ -2,11 +2,18 @@ package com.bandi.mhProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class MhProjectApplication {
+public class MhProjectApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		//외장 톰캣용 세팅
+		return builder.sources(MhProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MhProjectApplication.class, args);
