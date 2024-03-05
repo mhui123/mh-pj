8a5980b43c026d4158adcff0b17a6552fec28f23
mhui123 <mhui7864@gmail.com>
2024-03-05 21:12:13 +0900

    10분이하 표시방식 0추가

--- mh-front/src/utils/filters.js
+++ mh-front/src/utils/filters.js
@@ -6,7 +6,7 @@ function formatDate(value) {
   const dd = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
   let h = date.getHours();
   h = h > 9 ? h : `0${h}`;
-  const minute = date.getMinutes();
+  const minute = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`;
 
   return `${yyyy}-${mm}-${dd} ${h}:${minute}`;
 }
